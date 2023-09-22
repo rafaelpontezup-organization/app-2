7606ac603c3de7e76b265210c5b12518f2b42410
rafael.ponte@zup.com.br <rafael.ponte@zup.com.br>
2023-09-22 23:14:41 +0000

    stk-cli: create app initial commit.

---
+++ app-2/src/test/java/br/com/zup/edu/app2/App2ApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.zup.edu.app2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class App2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
